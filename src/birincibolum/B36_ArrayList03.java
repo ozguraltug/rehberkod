e235750eab34e2c8e7b2ef1b34bbad38f803e93f
ozguraltug <111081045+ozguraltug@users.noreply.github.com>
2024-05-01 10:41:27 +0300

    B36

---
+++ src/birincibolum/B36_ArrayList03.java
@@ -0,0 +1,12 @@
+package birincibolum;
+
+public class B36_ArrayList03 {
+    /*  Rastgele kullanici adi olusturan JAVA kodu yaziniz.
+     *  1. Kullanicidan ismini isteyelim
+     *  2. Kullanici adindaki bosluklari silelim.
+     *  3. Kullanici adinin alinabilir olup olmadigina bakalim.
+     *  4. Eger bizim listemizde oyle bir kullanici adi yoksa kullanici adi, kullanicinin girdigi isim olsun.
+     *  5. Eger bu kullanici adi zaten varsa, sonuna rastgele sayi olusturup ekleyelim, ve gosterelim.
+
+     */
+}
